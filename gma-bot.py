e09b5863aa84f5ea0b6c96349594d3fb16d7dd56
Vladislav Kuznetsov <starforce882@gmail.com>
2021-08-16 00:39:46 +0300

    Project finished

--- gma-bot.py
+++ gma-bot.py
@@ -19,16 +19,48 @@ logging.info('<---+--->\nStarting new session')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}! \n'
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! 😃\n'
                                       'Я бот, который кидает за вас кубики. И больше не надо лезть в интернет! \n'
                                       'На выбор доступны: \n'
-                                      '/d4,\n/d6,\n/d8,\n/d10,\n/d12,\n/d20,\n/d100\n', parse_mode='MARKDOWN')
+                                      '/d4    /d6    /d8    /d10    /d12    /d20    /d100')
+
+
+@bot.message_handler(commands=['help'])
+def start(message):
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! 😃\n'
+                                      'Ты и другие любители D&D можете кидать все нужные для игры кубики с помощью '
+                                      'одноимённых команд. Вот все доступные вам кубики:\n'
+                                      '/d4    /d6    /d8    /d10    /d12    /d20    /d100')
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def receive(call):
     if call.data == 'd4_repeat':
         d4(call.message)
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+    elif call.data == 'd6_repeat':
+        d6(call.message)
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+    elif call.data == 'd8_repeat':
+        d8(call.message)
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+    elif call.data == 'd10_repeat':
+        d10(call.message)
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+    elif call.data == 'd12_repeat':
+        d12(call.message)
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+    elif call.data == 'd20_repeat':
+        d20(call.message)
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+    elif call.data == 'd100_repeat':
+        d100(call.message)
         bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
 
 
@@ -41,7 +73,79 @@ def d4(message):
     bot.send_message(message.chat.id, '<strong>О, восславтесь же, Великие Боги Рандома!</strong>\n'
                                       f'D4: <strong>{dicenum}</strong> оказалось на верхней грани кубика.',
                      parse_mode='html', reply_markup=markup)
-    logging.info(f'D4: {dicenum}')
+    logging.info(f'D4: Someone rolled {dicenum}')
+
+
+@bot.message_handler(commands=['d6'])
+def d6(message):
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(text='🎲 Кинуть кубик ещё раз', callback_data='d6_repeat')
+    markup.add(btn)
+    dicenum = str(random.randint(1, 6))
+    bot.send_message(message.chat.id, '<strong>О, восславтесь же, Великие Боги Рандома!</strong>\n'
+                                      f'D6: <strong>{dicenum}</strong> оказалось на верхней грани кубика.',
+                     parse_mode='html', reply_markup=markup)
+    logging.info(f'D6: Someone rolled {dicenum}')
+
+
+@bot.message_handler(commands=['d8'])
+def d8(message):
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(text='🎲 Кинуть кубик ещё раз', callback_data='d8_repeat')
+    markup.add(btn)
+    dicenum = str(random.randint(1, 8))
+    bot.send_message(message.chat.id, '<strong>О, восславтесь же, Великие Боги Рандома!</strong>\n'
+                                      f'D8: <strong>{dicenum}</strong> оказалось на верхней грани кубика.',
+                     parse_mode='html', reply_markup=markup)
+    logging.info(f'D8: Someone rolled {dicenum}')
+
+
+@bot.message_handler(commands=['d10'])
+def d10(message):
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(text='🎲 Кинуть кубик ещё раз', callback_data='d10_repeat')
+    markup.add(btn)
+    dicenum = str(random.randint(1, 10))
+    bot.send_message(message.chat.id, '<strong>О, восславтесь же, Великие Боги Рандома!</strong>\n'
+                                      f'D10: <strong>{dicenum}</strong> оказалось на верхней грани кубика.',
+                     parse_mode='html', reply_markup=markup)
+    logging.info(f'D10: Someone rolled {dicenum}')
+
+
+@bot.message_handler(commands=['d12'])
+def d12(message):
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(text='🎲 Кинуть кубик ещё раз', callback_data='d12_repeat')
+    markup.add(btn)
+    dicenum = str(random.randint(1, 12))
+    bot.send_message(message.chat.id, '<strong>О, восславтесь же, Великие Боги Рандома!</strong>\n'
+                                      f'D12: <strong>{dicenum}</strong> оказалось на верхней грани кубика.',
+                     parse_mode='html', reply_markup=markup)
+    logging.info(f'D12: Someone rolled {dicenum}')
+
+
+@bot.message_handler(commands=['d20'])
+def d20(message):
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(text='🎲 Кинуть кубик ещё раз', callback_data='d20_repeat')
+    markup.add(btn)
+    dicenum = str(random.randint(1, 20))
+    bot.send_message(message.chat.id, '<strong>О, восславтесь же, Великие Боги Рандома!</strong>\n'
+                                      f'D20: <strong>{dicenum}</strong> оказалось на верхней грани кубика.',
+                     parse_mode='html', reply_markup=markup)
+    logging.info(f'D20: Someone rolled {dicenum}')
+
+
+@bot.message_handler(commands=['d100'])
+def d100(message):
+    markup = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(text='🎲 Кинуть кубик ещё раз', callback_data='d100_repeat')
+    markup.add(btn)
+    dicenum = str(random.randint(1, 100))
+    bot.send_message(message.chat.id, '<strong>О, восславтесь же, Великие Боги Рандома!</strong>\n'
+                                      f'D100: <strong>{dicenum}</strong> оказалось на верхней грани кубика.',
+                     parse_mode='html', reply_markup=markup)
+    logging.info(f'D100: Someone rolled {dicenum}')
 
 
 bot.polling(none_stop=True)
